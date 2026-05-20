201c95614e120afb214d9081012f87badd1d4ec4
Medidharaju-Lokeshwar-Raju <lokiwar14@gmail.com>
2026-05-20 10:05:51 +0530

    Initial commit - Student Grades Tracker

---
+++ src/com/studentgrades/StudentGradeManager.java
@@ -0,0 +1,77 @@
+package com.studentgrades;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Student {
+	String name;
+	int marks;
+
+	Student(String name, int marks) {
+		this.name = name;
+		this.marks = marks;
+	}
+}
+
+public class StudentGradeManager {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		ArrayList<Student> students = new ArrayList<>();
+
+		System.out.print("Enter number of students: ");
+		int n = sc.nextInt();
+		sc.nextLine();
+
+		// Input student details
+		for (int i = 0; i < n; i++) {
+
+			System.out.println("\nStudent " + (i + 1));
+
+			System.out.print("Enter name: ");
+			String name = sc.nextLine();
+
+			System.out.print("Enter marks: ");
+			int marks = sc.nextInt();
+			sc.nextLine();
+
+			students.add(new Student(name, marks));
+		}
+
+		// Calculations
+		int total = 0;
+		int highest = students.get(0).marks;
+		int lowest = students.get(0).marks;
+
+		for (Student s : students) {
+
+			total += s.marks;
+
+			if (s.marks > highest) {
+				highest = s.marks;
+			}
+
+			if (s.marks < lowest) {
+				lowest = s.marks;
+			}
+		}
+
+		double average = (double) total / n;
+
+		// Display Report
+		System.out.println("\n===== STUDENT REPORT =====");
+
+		for (Student s : students) {
+			System.out.println("Name: " + s.name + " | Marks: " + s.marks);
+		}
+
+		System.out.println("\n===== SUMMARY =====");
+		System.out.println("Average Marks : " + average);
+		System.out.println("Highest Marks : " + highest);
+		System.out.println("Lowest Marks  : " + lowest);
+
+		sc.close();
+	}
+}
